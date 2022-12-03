a04671d41c3b666647abda58df9286996601335b
Jesther Costinar <jesther.jc15@gmail.com>
2022-12-04 04:02:46 +0800

    Creating idea migration, models, controller and blade

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // Relationship with idea
+    public function idea() {
+        return $this->hasMany(Idea::class);
+    }
 }
